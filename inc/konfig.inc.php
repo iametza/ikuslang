b91ec9d261b598eff71ffcccc5089deb3093ce5b
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-03-31 12:29:43 +0200

    konfig.inc.php eguneratu2

--- inc/konfig.inc.php
+++ inc/konfig.inc.php
@@ -2,4 +2,7 @@
     // Garatzaile bakoitzaren url-ak.
 	define("URL_BASE_ADMIN", ADMINISTRAZIOKO ORRIAREN URL-A);
     define("URL_BASE", ALDE PUBLIKOAREN URL-A);
+    
+    // GCM gakoa
+    $apiKey = GCM-REN GAKOA;
 ?>
